f83a69a09862e1fb6e34b1f463961aac33e52b66
Shyam Kumar V N <vn.shyamkumar@gmail.com>
2020-08-26 17:36:31 +0530

    Initial Commit

---
+++ get_data.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import make_regression
+import pandas as pd
+import os
+import numpy as np
+
+if os.path.isfile("data.csv"):
+    n = 1
+else:
+    n = 50
+
+for i in range(0,n):
+    X,y = make_regression(10000,n_features=10)
+    df = pd.Dataframe(X)
+    df.to_csv("data.csv", mode='a')
